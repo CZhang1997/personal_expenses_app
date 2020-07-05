5ab94b029606e16eec82cd2c6938abab19e9b60f
Churong Zhang <42553484+CZhang1997@users.noreply.github.com>
2020-07-05 14:41:19 -0500

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "0527bc23a84d6faa48d2fbb6805fc1b6",
 "/": "0527bc23a84d6faa48d2fbb6805fc1b6",
-"main.dart.js": "841a8d4d96f3726b5f2b5ee3e6e7687d",
+"main.dart.js": "3735675a2521e36f680d9a111085d78c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
